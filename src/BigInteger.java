96d5f095970a056ef23322691432826fa9ec5fde
lukeqwaszx <lukeqwaszx@gmail.com>
2022-02-20 01:08:55 +0900

    The first draft of my data structure homework. It was a splendid job!

--- src/BigInteger.java
+++ src/BigInteger.java
@@ -1,17 +1,39 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.regex.*;
-  
+//  import java.util.regex.*;
+
+/*  Made on Feb 20, 2022.
+    The thing I regretted during my design is
+    that I could have made the BigInteger final.
+    Since there's no motivation to modify the BigInteger
+    object, I did not need to risk having unintended
+    side effects. Also, if the BigInteger was constant
+    I could keep the length of the byte array as a field,
+    not as a method to call. As the number was used across
+    various functions, keeping it as a variable would up
+    the performance quite a bit.
+
+    Also, naming skills still need much improvement.
+
+    One thing I also noticed is the importance of the classes
+    I took on Fall 2021. Chunking the codes into separate functions
+    was noticeably easy, and the importance of Discrete mathematics
+    course could be seen in modelling the problem.
+
+    Overall, the code contains improved design and
+    single responsibility principle was adhered to.
+    It is quite a step-up from my former project,
+    and I am proud for it. */
   
 public class BigInteger
 {
     public static final String QUIT_COMMAND = "quit";
     public static final String MSG_INVALID_INPUT = "입력이 잘못되었습니다.";
-    public static final Pattern EXPRESSION_PATTERN = Pattern.compile("[+\\-*]");
+    // public static final Pattern EXPRESSION_PATTERN = Pattern.compile("[+\\-*]");
 
     // Fields
     private boolean isNegative = false;
-    private byte[] digits = new byte[200];
+    private byte[] digits = new byte[201];
 
     // Constructors
     public BigInteger() {
@@ -33,46 +55,70 @@ public class BigInteger
         if (s.startsWith("-")) {
             isNegative = true;
             s = s.substring(1);
+        } else if (s.startsWith("+")) {
+            s = s.substring(1);
         }
         for (int j = 0; !(s.equals("") || s.equals("-")); j++, s = s.substring(0, s.length() - 1)) {
             digits[j] = (byte) Character.getNumericValue(s.charAt(s.length() - 1));
         }
-
     }
 
-    // Produce negative
+    // Produce clone
     public BigInteger(BigInteger a) {
         digits = a.digits;
+        isNegative = a.isNegative;
     }
 
     // Main operations
     public BigInteger add(BigInteger big) {
+        BigInteger value;
         if (isNegative == big.isNegative) {
-            BigInteger value = primitiveAdd(big);
+            value = primitiveAdd(big);
             value.isNegative = isNegative;
-        } else {
-            if (isAbsoluteBigger(big)) {
-                BigInteger value = primitiveSubtract(big);
-                value.isNegative = isNegative;
-            } else {
-                BigInteger value = big.primitiveSubtract(this);
+        } else if (isAbsoluteBigger(big)) {
+            value = primitiveSubtract(big);
+            value.isNegative = isNegative;
+        }
+        else {
+                value = big.primitiveSubtract(this);
                 value.isNegative = big.isNegative;
             }
-        }
+        return value;
     }
 
     public BigInteger subtract(BigInteger big) {
-        BigInteger negation = big.clone();
+        BigInteger negation = new BigInteger(big);
+        negation.isNegative = !(big.isNegative);
+        return add(negation);
     }
 
     public BigInteger multiply(BigInteger big) {
-
+        BigInteger value = new BigInteger();
+        for (int i = 0; i <= big.endIndex(); i++) {
+            value = value.primitiveAdd(this.primitiveMultiply(big.digits[i], i));
+        }
+        if (isNegative != big.isNegative) {
+            value.isNegative = true;
+        }
+        return value;
     }
 
     @Override
     public String toString() {
-
+        int myLength = endIndex();
+        if (myLength == -1) {
+            return "0";
+        }
+        StringBuilder builder = new StringBuilder();
+        if (isNegative) {
+            builder.append("-");
+        }
+        for (int i = myLength; i >= 0; i--) {
+            builder.append(digits[i]);
+        }
+        return builder.toString();
     }
+
     // Helper methods
     private BigInteger primitiveAdd(BigInteger big) {
         BigInteger newBig = new BigInteger();
@@ -92,9 +138,20 @@ public class BigInteger
                 carry = -1;
             } else {
                 newBig.digits[i] = (byte) (digits[i] + carry - big.digits[i]);
-                carry = 0
+                carry = 0;
             }
         } return newBig;
+    }
+
+    private BigInteger primitiveMultiply(int factor, int power) {
+        BigInteger newBig = new BigInteger();
+        int myLength = endIndex();
+        for (int i = 0, carry = 0; i <= myLength + 1; i++) {
+            int result = digits[i] * factor + carry;
+            carry = result / 10;
+            newBig.digits[i + power] = (byte) (result % 10);
+        }
+        return newBig;
     }
 
     private boolean isAbsoluteBigger(BigInteger big) {
@@ -127,15 +184,26 @@ public class BigInteger
 
     static BigInteger evaluate(String input) throws IllegalArgumentException
     {
-        // implement here
-        // parse input
-        // using regex is allowed
-  
-        // One possible implementation
-        // BigInteger num1 = new BigInteger(arg1);
-        // BigInteger num2 = new BigInteger(arg2);
-        // BigInteger result = num1.add(num2);
-        // return result;
+        input = input.replace(" ", "");
+        int operatorIndex = findOperatorIndex(input);
+        BigInteger one = new BigInteger(input.substring(0, operatorIndex));
+        BigInteger two = new BigInteger(input.substring(operatorIndex + 1));
+        char operator = input.charAt(operatorIndex);
+        return switch (operator) {
+            case '+' -> one.add(two);
+            case '-' -> one.subtract(two);
+            case '*' -> one.multiply(two);
+            default -> null;
+        };
+    }
+
+    static int findOperatorIndex(String input) {
+        for (int i = 1; i < input.length(); i++) {
+            if (!Character.isDigit(input.charAt(i))) {
+                return i;
+            }
+        }
+        return -1;
     }
   
     public static void main(String[] args) throws Exception
